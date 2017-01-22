5580c58ca41f46600cd80a85fa380d67fb574b27
Ajay Raj <ajay@Ajays-MacBook-Pro.local>
2017-01-21 22:51:01 -0800

    bootstrap

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def diary():
+    return render_template('templates/diary.html', name=name)
 
 if __name__ == "__main__":
     app.run()
