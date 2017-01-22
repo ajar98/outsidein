a73785864f24588aa1fc80b8f59f4fc7f815ebf6
Ajay Raj <ajay@Ajays-MacBook-Pro.local>
2017-01-21 23:18:25 -0800

    form

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
 app = Flask(__name__)
 
-@app.route("/")
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+
+@app.route("/", methods=['GET', 'POST'])
 def diary():
-    return render_template('templates/diary.html', name=name)
+    form = ReusableForm(request.form)
+    print(form.errors)
+    if request.method == 'POST':
+        name = request.form['name']
+        print(name)
+
+        if form.validate():
+            # Save the comment here.
+            return name
+        else:
+            return 'All the form fields are required. '
+    else:
+        return render_template('diary.html', form=form)
 
 if __name__ == "__main__":
     app.run()
